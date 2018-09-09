13bfc437f056dd042c66b12b3a0051765c191a58
cwcsp1@163.com <a19900809>
2018-09-10 00:10:23 +0800

    [^]使用DatabaseHelper实现方法

--- chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
+++ chapter2/src/main/java/org/smart4j/chapter2/service/CustomerService.java
@@ -27,17 +27,14 @@ public class CustomerService {
     }
 
     public boolean createCustomer(Map<String, Object> fieldMap) {
-        // TODO
-        return false;
+        return DatabaseHelper.insertEntity(Customer.class, fieldMap);
     }
 
     public boolean updateCustomer(long id, Map<String, Object> fieldMap) {
-        // TODO
-        return false;
+        return DatabaseHelper.updateEntity(Customer.class, id, fieldMap);
     }
 
     public boolean deleteCustomer(long id) {
-        // TODO
-        return false;
+        return DatabaseHelper.deleteEntity(Customer.class, id);
     }
 }
