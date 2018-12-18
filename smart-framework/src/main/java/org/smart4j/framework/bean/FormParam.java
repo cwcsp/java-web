5d470978d9993345d2c0b65b17a87cd0cb18f089
caoweichao <weichaocao@sf-express.com>
2018-12-18 10:13:54 +0800

    [+]Chapter5

---
+++ smart-framework/src/main/java/org/smart4j/framework/bean/FormParam.java
@@ -0,0 +1,28 @@
+package org.smart4j.framework.bean;
+
+public class FormParam {
+
+    private String fieldName;
+    private Object fieldValue;
+
+    public FormParam(String fieldName, Object fieldValue) {
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
+    public Object getFieldValue() {
+        return fieldValue;
+    }
+
+    public void setFieldValue(Object fieldValue) {
+        this.fieldValue = fieldValue;
+    }
+}
--- smart-framework/src/main/java/org/smart4j/framework/util/StringUtil.java
+++ smart-framework/src/main/java/org/smart4j/framework/util/StringUtil.java
@@ -4,6 +4,11 @@ import org.apache.commons.lang3.StringUtils;
 
 public final class StringUtil {
 
+    /**
+     * 字符串分隔符
+     */
+    public static final String SEPARATOR = String.valueOf((char)29);
+
     public static boolean isEmpty(String str) {
         if(str != null) {
             str = str.trim();
